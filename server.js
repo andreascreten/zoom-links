3385fbcbc9b16062e35a4eead8ff88a5b298857d
Andreas Creten <andreas@madewithlove.be>
2017-08-29 10:03:11 +0200

    More loggin

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ slapp.message('^([0-9-]{9,12})$', (msg) => {
 })
 
 slapp.message('^\<tel\:[0-9-]{9,12}\|([0-9-]{9,12})\>', (msg) => {
-  console.log(msg);
+  console.log(msg.body.event.text.replace('-', '').replace('>', ''));
   msg.say('https://teamleader.zoom.us/j/' + msg.body.event.text.replace('-', '').replace('>', '').replace('/^\<tel\:[0-9]{9,12}\|', ''))
 })
 
